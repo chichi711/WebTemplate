5a300fbfd88765752fcbf4c7b740909f319e1177
unknown <ga899893@gmail.com>
2020-07-14 20:58:43 +0800

    buildPageVS2

---
+++ WebTemPage Builder/public/javascripts/js/buildEp2.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+   });
+
+
+   $("#copyBtn").click(function() {
+    var name = $(this).attr('name');
+    var el = document.getElementById(name);
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+    document.execCommand('copy');
+    alert("複製成功");
+    return false;
+  });
+
+});
