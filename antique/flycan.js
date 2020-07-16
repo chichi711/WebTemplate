84955069d3bc80de7ba451aff143a30dddf29c84
名稱 <>
2020-07-16 17:35:42 +0800

    new

---
+++ antique/flycan.js
@@ -0,0 +1,53 @@
+$(function() {
+
+    $("#SMALL img").click(function() {
+
+        var N = $(this).attr("id").substr(2);
+
+        $("#BIG").attr("src", "image/Xiang se/xiang_duck" + N + ".jpg");
+
+    });
+
+
+    //導覽頁變化
+    $(window).scroll(function() {
+        var scrollVal = $(this).scrollTop(); //捲動
+
+
+        if (scrollVal > 100) {
+            /* 滾動的物件捲動 > 100 就觸發。*/
+            // $("#logo").slideUp(100);  上滑
+            $(".title").addClass("active");
+        } else {
+            // $("#logo").slideDown(100); 下滑
+            $(".title").removeClass("active"); //刪掉
+        }
+    });
+
+    //大圖片變化
+    var x = 0
+    setInterval(function() {
+        if (x == 3) {
+            x = 0
+            $(".cot4").find("img").eq(x).addClass("change");
+            $(".cot4").find("img").eq(x + 3).removeClass("change");
+
+        } else {
+
+            $(".cot4").find("img").eq(x + 1).addClass("change");
+            $(".cot4").find("img").eq(x).removeClass("change");
+            x++
+        }
+
+        // $(".cot4").find("img").eq(x + 1).addClass("change");
+        // $(".cot4").find("img").eq(x).removeClass("change");
+
+    }, 3500);
+
+
+
+
+});
+
+//換圖片 
+//substr 選第二位
