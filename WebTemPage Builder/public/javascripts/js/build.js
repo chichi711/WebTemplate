1e78f0b2f21c7968a5a1e9f0545554b489c5f955
unknown <ga899893@gmail.com>
2020-07-20 17:51:49 +0800

    0720

--- WebTemPage Builder/public/javascripts/js/build.js
+++ WebTemPage Builder/public/javascripts/js/build.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
 
-    $("#sidebar").mCustomScrollbar({
+    $("#sidebar,#sidebarRight").mCustomScrollbar({
         theme: "minimal"
    });
 
    $('#sidebarCollapse').on('click', function () {
        // open or close navbar
        $('#sidebar,#content').toggleClass('active');
+       // close dropdowns
+       $('.collapse.in').toggleClass('in');
+       // and also adjust aria-expanded attributes we use for the open/closed arrows
+       // in our CSS
+       $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+   });
+   $('#sidebarCollapseRight').on('click', function () {
+       // open or close navbar
+       $('#sidebarRight').toggleClass('active');
        // close dropdowns
        $('.collapse.in').toggleClass('in');
        // and also adjust aria-expanded attributes we use for the open/closed arrows
@@ -23,7 +32,6 @@ $(document).ready(function () {
     sel.removeAllRanges();
     sel.addRange(range);
     document.execCommand('copy');
-    alert("複製成功");
     return false;
 
     
