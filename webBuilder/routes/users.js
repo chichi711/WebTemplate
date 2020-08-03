4e0284263ccd833675f03f8fb65c483c2ffa206c
unknown <ga899893@gmail.com>
2020-08-03 15:57:39 +0800

    loginTest

--- webBuilder/routes/users.js
+++ webBuilder/routes/users.js
@@ -12,6 +12,36 @@ router.get('/login/profile', function (req, res, next) {
   res.render('profile');
 });
 
+router.get("/member", function (request, response) {
+
+  conn.query('select * from member',
+      '',
+      function (err, rows) {
+          if (err) {
+              console.log(JSON.stringify(err));
+              return;
+          }
+
+          response.send(JSON.stringify(rows));
+      }
+  );
+
+})
+// 註冊
+router.post("/member", function (request, response) {
+
+	conn.query(
+		"insert into member set uName = ?,Email = ?, uPwd = ?",
+			[
+				request.body.uName,
+				request.body.Email,
+				request.body.uPwd
+			]);
+	response.send("row inserted.");
+
+})
+
+// 登入驗證
 
 
 
