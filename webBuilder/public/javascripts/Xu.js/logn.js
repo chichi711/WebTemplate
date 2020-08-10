efa5902b1a2c7a6c11f41bc44e56eb17b43f0767
YuWei <ga899893@gmail.com>
2020-08-11 00:46:38 +0800

    signup 格式 profile 卷軸

--- webBuilder/public/javascripts/Xu.js/logn.js
+++ webBuilder/public/javascripts/Xu.js/logn.js
@@ -9,44 +9,57 @@ $(".txtb input").on("blur", function () {
         $(this).removeClass("focus");
 });
 
-function autoinput (type) {
-    $(".tip").hide();
-    if(type == 'sign up') {
-        let num = 0;
-        let value = ['yoru', 'yoru123@gmail.com', 'qqq', 'qqq'];
-        $('form input:not(:last-child)').each( function () {
-            $(this).addClass("focus");
-            $(this).val(value[num]);
-            num++;
-        });
-    }
-    if(type == 'sign in') {
-        let num = 0;
-        let value = ['yoru123@gmail.com', 'qqq'];
-        $('form input:not(:last-child)').each( function () {
-            $(this).addClass("focus");
-            $(this).val(value[num]);
-            num++;
-        });
-    }
-}
-$(".tip").hide();
-function checkpas() {
-    var pas1 = document.getElementById("uPwdTextBox").value;
-    var pas2 = document.getElementById("repassword").value;
-    if (pas1 != pas2 && pas2 != "") {
-        $(".tip").show();//當兩個密碼不相等時則顯示錯誤資訊
-    } else {
+// function autoinput (type) {
         $(".tip").hide();
-    }
-}
+        $(".tip2").hide();
+        $(".tip3").hide();
+        function checkpas3() {//失去焦點
+            var pas4 = document.getElementById("uNameTextBox").value;
+            if (pas4 == "")
+                $(".tip3").show();
+            else
+                $(".tip3").hide();
+        }
+        function checkpas2() {
+            var pas3 = document.getElementById("EmailTextBox").value;
+            if (pas3 == "")
+                $(".tip2").show();
+            else
+                $(".tip2").hide();
+        }
+        function checkpas1() {//失去焦點
+            var pas1 = document.getElementById("uPwdTextBox").value;
+            var pas2 = document.getElementById("repassword").value;
+            if (pas1 != pas2 && pas2 != "")
+                $(".tip").show();
+            else
+                $(".tip").hide();
+        }
+        function checkpas() {
+            var pas1 = document.getElementById("uPwdTextBox").value;
+            var pas2 = document.getElementById("repassword").value;
 
-$("#okButton").click(function () {
-    var pas3 = document.getElementById("uPwdTextBox").value;
-    var pas4 = document.getElementById("repassword").value;
-    if (pas3 != pas4) {
-        $(".tip").show();
-    }else{
+            if (pas1 != pas2) {
+                $(".tip").show();
+            } else {
+                $(".tip").hide();
+            }
+        }
+
+        $("#okButton").click(function () {
+            var pas3 = document.getElementById("uPwdTextBox").value;
+            var pas4 = document.getElementById("repassword").value;
+            var pas4 = document.getElementById("uNameTextBox").value;
+            var pas3 = document.getElementById("EmailTextBox").value;
+            if (pas3 == "" || pas4 == "") {
+                alert("請輸入資料");
+                return false;
+            }
+
+            if (pas3 != pas4) {//當兩個密碼不相等時則顯示錯誤資訊
+                alert("兩次輸入的密碼不一致！");
+                return false;
+            }
         var newItem = {
             uName: $("#uNameTextBox").val(),
             Email: $("#EmailTextBox").val(),
@@ -59,5 +72,6 @@ $("#okButton").click(function () {
             data: newItem
         })
         window.location.href = window.location.origin + '/user/login';
-    }
-})
+    })
+    
+// }
